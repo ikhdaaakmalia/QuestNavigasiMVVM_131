6bf95a95b976c1a371c406f469d39df6fa034e2d
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-21 17:10:26 +0700

    membuat enum class halaman yang berisi formulir & tampil data

---
+++ app/src/main/java/com/example/arsitekturmvvm/NavigationControl.kt
@@ -0,0 +1,24 @@
+package com.example.arsitekturmvvm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm.model.JenisKelamin.jenisK
+import com.example.arsitekturmvvm.ui.view.FormulirView
+import com.example.arsitekturmvvm.ui.view.TampilData
+import com.example.arsitekturmvvm.ui.view.TampilDataView
+import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
+
+enum class Halaman {                                     //menandai nama per halaman
+    FORMULIR,
+    TAMPILDATA
+}
+
