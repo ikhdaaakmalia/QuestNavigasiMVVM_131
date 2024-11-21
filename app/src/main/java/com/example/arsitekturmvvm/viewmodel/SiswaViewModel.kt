b836ade5220a04f974e96d1eb608fd7733047634
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-21 17:14:11 +0700

    membuat fungsi save DataSiswaViewModel

--- app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/arsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -11,5 +11,16 @@ class SiswaViewModel: ViewModel(){                                      //extend
     private val _statusUI = MutableStateFlow(DataSiswa())               //_ : hanya bisa diakses oleh class itu sendiri (private)
     val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()        //statusUI tdk private agar bisa diakses oleh view
 
-
+    fun saveDataSiswa(ls: MutableList<String>){                         //memiliki parameter list dan didalamnya ada update
+        _statusUI.update {statusSaatIni ->                              //update artinya status saat ini isinya adalah sebuah model. yg di update ya model nya: nama, gender, alamat
+            statusSaatIni.copy(
+                nama = ls[0],                                           //ls : didapat dari UI
+                nim = ls[1],
+                gender = ls[2],
+                email = ls[3],
+                alamat = ls[4],
+                noTelp = ls[5]
+            )
+        }
+    }
 }
