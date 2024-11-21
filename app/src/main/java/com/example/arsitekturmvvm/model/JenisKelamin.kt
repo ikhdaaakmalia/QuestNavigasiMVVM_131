a9ff991d1a14e106c9662a8613d1675e7f6bd9dd
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-21 17:09:19 +0700

    membuat object jenis kelamin untuk radionya

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/JenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.arsitekturmvvm.model
+
+import com.example.arsitekturmvvm.R
+
+object JenisKelamin {               //object digunakan untuk membuat dummy(salinan) data
+    val jenisK = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
