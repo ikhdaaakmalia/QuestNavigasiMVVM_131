570a98298aa2184d57cd1e112365a1c297bf0530
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-21 16:58:43 +0700

    membuat fungsi formulir view

---
+++ app/src/main/java/com/example/arsitekturmvvm/ui/view/FormulirView.kt
@@ -0,0 +1,34 @@
+package com.example.arsitekturmvvm.ui.view
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+//@Preview(showBackground = true)
+@Composable
+fun FormulirView(
+    modifier: Modifier = Modifier,
+    listJK: List<String>,       //akan dipanggil function formulir view
+    onSubmitClicked:(MutableList<String>) -> Unit) //unit untuk menghandel on click/on submit tipe data dlm parameter
+{
+
+}
