6edd7dc918f95cf518deff8bf6d586eceec735ae
ikhdaakmalia <punyaamelaja@gmail.com>
2024-11-21 16:57:22 +0700

    memberikan val pada class DataSiswa untuk memberikan nilai

--- app/src/main/java/com/example/arsitekturmvvm/model/DataSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataSiswa.kt
@@ -1,5 +1,10 @@
 package com.example.arsitekturmvvm.model
 
 data class DataSiswa(                        //kurung biasa karena akan dibuat properti dlm bentuk variabel
-
+    val nama: String="",                    //val karena nilainya tetep 1
+    val nim: String="",
+    val email: String="",
+    val alamat: String="",
+    val noTelp: String="",
+    val gender: String="",
 )
